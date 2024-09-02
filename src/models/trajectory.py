4d9b41e9c2a951236a2074a411903b17293879d8
llmvetter <lennart.vetter@yahoo.de>
2024-09-02 14:13:13 +0200

    Refactors Trajectory class

--- src/models/trajectory.py
+++ src/models/trajectory.py
@@ -1,17 +1,23 @@
-from lunar_lander.src.models.state_action import StateActionPair
+from car_following.src.models.state_action import StateActionPair
 
+class Trajectory:
+    def __init__(self, speed, distance, acceleration):
+        if not (len(speed) == len(distance) == len(acceleration)):
+            raise ValueError("Speed, distance, and acceleration must have the same length")
 
-class Trajectory():
-    def __init__(self, trajectory):
-        self.trajectory = self.validate(trajectory)
-    def validate(self, feature):
-        if isinstance(feature, list):
-            return feature
-        else:
-            return ValueError('not of type list[StateActionPair]')
-        
-    def discretize(self):
-        for state_action_pair in self.trajectory:
-            state_action_pair.state = StateActionPair.clip_state(
-                state_action_pair.state
+        self.trajectory = []
+        for s, d, a in zip(speed, distance, acceleration):
+            pair = StateActionPair(
+                state=(s, d),
+                action=a
             )
+            self.trajectory.append(pair)
+
+    def __len__(self):
+        return len(self.trajectory)
+
+    def __getitem__(self, index):
+        return self.trajectory[index]
+
+    def __iter__(self):
+        return iter(self.trajectory)
