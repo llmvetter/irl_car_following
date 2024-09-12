4ce736186995d56591f28990c8738d619deca9c1
llmvetter <lennart.vetter@yahoo.de>
2024-09-12 13:17:37 +0200

    Introduces forward pass

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.special import logsumexp
+from scipy import sparse
 
 from car_following.src.models.mdp import CarFollowingMDP
 from lunar_lander.src.models.trajectories import Trajectories
@@ -23,6 +24,17 @@ def policy_state_visitation_frequency(
 ):
     pass
 
+def terminal_probabilities_from_trajectories(
+        trajectories: Trajectories,
+        n_states: int,
+) -> np.ndarray:
+    p = np.zeros(n_states)
+    for trajectory in trajectories:
+        terminal_state = trajectory.trajectory[-1].state.index
+        p[terminal_state] += 1.0
+
+    return p/len(trajectories.trajectories)
+
 def initial_probabilities_from_trajectories(
         trajectories: Trajectories,
         n_states: int,
@@ -41,13 +53,13 @@ def compute_action_probability(
 ) -> np.ndarray:
     
     n_states = mdp.n_states
-    n_actions = len(mdp.action_space)
+    n_actions = mdp.n_actions
     
     # Compute rewards in log space
     log_rewards = np.array([reward.get_reward(s) for s in range(n_states)])
     
     # Initialize log_zs based on frequency of end states in trajectories
-    log_zs = initial_probabilities_from_trajectories(
+    log_zs = terminal_probabilities_from_trajectories(
         trajectories=trajectories,
         n_states = n_states,
     ) #thi should be end states not starting states
@@ -67,7 +79,30 @@ def compute_action_probability(
     log_p_action = log_za - logsumexp(log_za, axis=1)[:, None]
     p_action = np.exp(log_p_action)
     
-    return p_action
+    # return p_action
+
+    p_transition = np.zeros((mdp.n_states, mdp.n_states, mdp.n_actions))
+
+    for (s_from, a), s_to in mdp.T.items():
+        p_transition[s_from, s_to, a] = 1
+
+    p_initial = initial_probabilities_from_trajectories(
+        trajectories,
+        n_states,
+    )
+
+    p_transition_sparse = sparse.csr_matrix(p_transition.reshape(n_states * n_actions, n_states))
+    p_action_sparse = sparse.csr_matrix(p_action)
+    d = sparse.csr_matrix((n_states, n_states))
+    d[:, 0] = p_initial
+
+    for t in range(1, 500):
+        s_a_probs = d[:, t-1].multiply(p_action_sparse)
+        s_a_probs_r = s_a_probs.reshape(1, -1)
+        d[:, t] = s_a_probs_r.dot(p_transition_sparse).T
+
+    state_frequencies = d.sum(axis=1).A1
+    return state_frequencies
 
 def plot_heatmap(
         trajectories: Trajectories,
