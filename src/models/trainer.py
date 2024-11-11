01894e8df9bcc2e1acbaa1f1c8e593f90b69418e
llmvetter <lennart.vetter@yahoo.de>
2024-11-11 14:19:01 +0100

    Refactors for epochs instead of convergence criterion

--- src/models/trainer.py
+++ src/models/trainer.py
@@ -32,6 +32,7 @@ class Trainer:
     
     def train(
             self,
+            epochs: int = 20,
     ) -> np.ndarray:
 
         expert_svf = svf_from_trajectories(
@@ -39,23 +40,28 @@ class Trainer:
             mdp=self.mdp,
         )
 
-        delta = np.inf
+        for _ in range(epochs):
 
-        while delta > self.eps:
-
-            logging.info("Backwardpass")
+            logging.info("Entering Backwardpass")
             policy: torch.tensor = backward_pass(
                 mdp=self.mdp,
                 reward_func=self.reward_function,
+                gamma=0.98,
+                max_iterations=50,
+                temperature=2,
+
             )
-            logging.info("Forward Pass")
+            logging.info("Entering Forward Pass")
             expected_svf: np.ndarray = forward_pass(
                 mdp=self.mdp,
                 policy=policy,
+                iterations=100,
             )
             #calculate feature expectation from svf
             grad = np.dot((expert_svf - expected_svf), self.mdp.state_space)
-            logging.info(f'Expected feature expectation: {np.dot(expected_svf, self.mdp.state_space)}')
+            logging.info(
+                f'Expected feature expectation: {np.dot(expected_svf, self.mdp.state_space)}'
+            )
 
             # perform optimization step and compute delta for convergence
             loss = self.optimizer.step(torch.tensor(grad, dtype=torch.float32))
