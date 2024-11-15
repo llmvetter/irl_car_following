201f82977c7876e6f326f76ff5f8fa857256041c
llmvetter <lennart.vetter@yahoo.de>
2024-11-15 14:26:24 +0100

    Increases backward pass iterations

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ class Config:
             "epsilon": 0.5,
             "discount": 0.95,
             "temperature": 0.7,
-            "iterations": 50,
+            "iterations": 100,
         }
         self.forward_pass ={
             "iterations": 1000,
@@ -16,4 +16,4 @@ class Config:
             "learning_rate":0.001,
             "layers":[30,90,20]
         }
-        self.epochs = 15
+        self.epochs = 10
