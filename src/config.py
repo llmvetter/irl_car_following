77af4c9237de61d6ff2ce60828c92b14bf8969bb
llmvetter <lennart.vetter@yahoo.de>
2024-11-21 14:44:56 +0100

    Sets params for testing

--- src/config.py
+++ src/config.py
@@ -16,14 +16,14 @@ class Config:
             "epsilon": 0.5,
             "discount": 0.95,
             "temperature": 0.7,
-            "iterations": 80
+            "iterations": 1
         }
         self.forward_pass ={
-            "iterations": 200,
+            "iterations": 1,
             "steps": 2000
         }
         self.reward_network = {
             "learning_rate":0.0005,
             "layers":[30,90,20]
         }
-        self.epochs = 8
+        self.epochs = 1
