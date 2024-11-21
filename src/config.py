3233d5e7d3ba70b8419694f7660060755cfd935f
llmvetter <lennart.vetter@yahoo.de>
2024-11-21 11:14:42 +0100

    Sets training params

--- src/config.py
+++ src/config.py
@@ -6,21 +6,21 @@ class Config:
             "a_min":-1.5,
             "a_max":2,
             "a_steps":0.75,
-            "v_steps":0.25,
+            "v_steps":0.1,
             "g_steps":0.25
         }
         self.backward_pass = {
             "epsilon": 0.5,
-            "discount": 0.97,
+            "discount": 0.95,
             "temperature": 0.7,
-            "iterations": 1
+            "iterations": 80
         }
         self.forward_pass ={
-            "iterations": 1,
+            "iterations": 200,
             "steps": 2000
         }
         self.reward_network = {
             "learning_rate":0.0005,
-            "layers":[50,50,50]
+            "layers":[50,80,50]
         }
-        self.epochs = 1
+        self.epochs = 8
