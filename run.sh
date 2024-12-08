6024ed6279cdc6602d9c80e7591012a2b6424d3b
llmvetter <lennart.vetter@yahoo.de>
2024-12-08 12:18:46 +0100

    Adds policy extractor

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=50
 #SBATCH --mem-per-cpu=6000
 #SBATCH --nodes=1
-#SBATCH --time=30:00:00
+#SBATCH --time=02:00:00
 #SBATCH -J jobname
 #SBATCH --error="/home/leve469a/myjob-%J.out"
 #SBATCH --output="/home/leve469a/myjob-%J.out"
@@ -14,4 +14,4 @@ ml purge
 ml release/24.04  GCC/13.2.0 OpenMPI/4.1.6 SciPy-bundle/2023.11
 ml PyTorch/2.1.2
 
-srun python /home/leve469a/car_following/runner.py
+srun python /home/leve469a/car_following/policy.py
