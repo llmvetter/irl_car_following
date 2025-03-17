eff1392ea77130d400f804f589c2e90964c9c45e
llmvetter <lennart.vetter@yahoo.de>
2025-03-17 23:31:06 +0100

    Sets training param

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ class Config:
         }
         self.dataset_path = '/home/h6/leve469a/IQ-Learn/data/LF_data-2e5.csv'
         self.mdp = {
-            "actions": [-10, -6, -3, -1.6, -0.8, -0.4, 0, 0.4, 0.8, 1.6, 3, 6, 10],
+            "actions": [-5, -3, -1.6, -0.8, -0.4, -0.2, 0, 0.2, 0.4, 0.8, 1.6, 3, 5],
             "max_speed": 30,
             "max_distance": 100,
             "max_rel_speed": 30,
@@ -16,16 +16,16 @@ class Config:
         }
         self.backward_pass = {
             "epsilon": 0.5,
-            "discount": 0.97,
+            "discount": 0.98,
             "temperature": 0.7,
             "iterations": 80
         }
         self.forward_pass ={
-            "iterations": 50,
-            "steps": 1000
+            "iterations": 100,
+            "steps": 500
         }
         self.reward_network = {
             "learning_rate":0.001,
             "layers":[50,50,50]
         }
-        self.epochs = 10
+        self.epochs = 30
