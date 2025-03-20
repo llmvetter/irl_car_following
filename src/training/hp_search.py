24ff2a960dd316e33f15813c64b5151fb3056570
llmvetter <lennart.vetter@yahoo.de>
2025-03-20 12:43:35 +0100

    Implements raytune job

---
+++ src/training/hp_search.py
@@ -0,0 +1,84 @@
+import logging
+import sys
+import numpy as np
+import pandas as pd
+
+import ray
+from ray import tune
+import gymnasium as gym
+from omegaconf import OmegaConf
+
+from src.utils import deep_merge
+from src.models.env import CarFollowingEnv
+from src.training.objective import objective
+
+# Init config
+config = OmegaConf.load('/home/h6/leve469a/IQ-Learn/config.yaml')
+config_dict = OmegaConf.to_container(config, resolve=True)
+
+# Hyperparameter search space
+search_space = {
+    'backward_pass': {
+        'epsilon': tune.uniform(0.1, 0.9),
+        'discount': tune.uniform(0.95, 0.99),
+        'temperature': tune.uniform(0.5, 1.0),
+    },
+    'reward_network': {
+        'learning_rate': tune.loguniform(1e-4, 1e-2),
+        'layers': tune.choice([[32, 32], [64, 64], [128, 128]]),
+    },
+}
+
+task_id = int(sys.argv[1]) - 1
+
+def create_name(trial):
+    return f"trial_{trial.trial_id}"
+
+# precompute and save transition matrix
+gym.register(
+    id="CarFollowing",
+    entry_point=CarFollowingEnv,
+)
+env = gym.make(
+        id="CarFollowing",
+        dataset_path=config.data.exp_path,
+        granularity=config.env.granularity,
+        actions=config.env.actions,
+        max_speed=config.env.max_speed,
+        max_distance=config.env.max_distance,
+        max_rel_speed=config.env.max_rel_speed,
+        delta_t=config.env.delta_t,
+    )
+env = env.unwrapped
+env.reset()
+
+logging.info("Computing transition probability matrix")
+env.compute_transitions()
+np.save(config.data.trans_path, env.T)
+
+# initialize tuning job
+logging.info("Initializing tuner")
+ray.init(logging_level=logging.WARN)
+
+tuner = tune.Tuner(
+    tune.with_parameters(objective),
+    param_space=deep_merge(config_dict, search_space),
+    tune_config=tune.TuneConfig(
+        metric="score",
+        mode="max",
+        num_samples=100,
+        trial_dirname_creator=create_name,
+    ),
+)
+
+# Run tuning
+results = tuner.fit()
+
+# Get best config
+best_result = results.get_best_result(metric='score', mode='min')
+df: pd.DataFrame = results.get_dataframe(filter_metric="score", filter_mode="min")
+df_sorted = df.sort_values(by="score", ascending=True)
+best_config = best_result.config if best_result else None
+
+print(f'Best config found: {best_config}')
+print(df_sorted.head(5))
---
+++ src/training/objective.py
@@ -0,0 +1,96 @@
+import logging
+from typing import Any
+
+import gymnasium as gym
+from ray import tune
+
+from src.models.trainer import Trainer
+from src.models.agent import Agent
+from src.models.evaluator import Evaluator
+from src.models.optimizer import GradientAscentOptimizer
+from src.models.reward import RewardNetwork
+from src.models.preprocessor import MilanoPreprocessor
+from src.models.env import CarFollowingEnv
+from src.config import DotDict
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+gym.register(
+    id="CarFollowing",
+    entry_point=CarFollowingEnv,
+)
+
+def objective(config: dict[str, Any]):
+
+    config = DotDict(config)
+    logging.info(f"loaded config with params: {config.__dict__}")
+
+
+    logging.info("Initializing Environment")
+    env = gym.make(
+        id="CarFollowing",
+        dataset_path=config.data.exp_path,
+        granularity=config.env.granularity,
+        actions=config.env.actions,
+        max_speed=config.env.max_speed,
+        max_distance=config.env.max_distance,
+        max_rel_speed=config.env.max_rel_speed,
+        delta_t=config.env.delta_t,
+    )
+    env = env.unwrapped
+    env.reset()
+
+    logging.info("Loading transition probability matrix")
+    env.load_transitions(config.data.trans_path)
+
+    logging.info("Init Reward Function")
+    reward_function = RewardNetwork(
+        mdp=env,
+        layers=config.reward_network.layers)
+
+    logging.info("Init Optimizer")
+    optimizer = GradientAscentOptimizer(
+        mdp=env,
+        reward_network=reward_function,
+        lr=config.reward_network.learning_rate,
+    )
+
+    logging.info("Loading Trajectories")
+    expert_trajectories = MilanoPreprocessor(
+        mdp=env,
+        config=config,
+    ).load(path=config.data.exp_path)
+
+    logging.info("Init Trainer")
+    trainer = Trainer(
+            config,
+            expert_trajectories,
+            optimizer,
+            reward_function,
+            env,
+    )
+
+    logging.info("Init IRL Loop")
+    _, policy = trainer.train(
+        epochs=config.epochs,
+    )
+
+    logging.info("Initializing Evaluator")
+    agent = Agent(
+        policy=policy,
+        env=env,
+    )
+
+    evaluator = Evaluator(
+        environment=env,
+        agent=agent,
+    )
+
+    logging.info("Initializing evaluation process")
+    metrics = evaluator.evaluate(
+        num_trajectories=100,
+    )
+    tune.report({'score': metrics['final_score']})
+
+    return {'score': metrics['final_score']}
