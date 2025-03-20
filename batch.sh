b0926de3795be2ac124cd6c76df0e228035dfdd8
llmvetter <lennart.vetter@yahoo.de>
2025-03-20 12:47:35 +0100

    Implements batch job shell script

---
+++ batch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=9000
+#SBATCH --nodes=1
+#SBATCH --time=02:00:00
+#SBATCH --error="/home/leve469a/maxent_batch-%A_%a.out"
+#SBATCH --output="/home/leve469a/maxent_batch-%A_%a.out"
+#SBATCH --array=1-4
+
+ml purge
+ml release/24.04 GCCcore/11.3.0
+ml Python/3.10.4
+
+source /home/h6/leve469a/IQ-Learn/.venv/bin/activate
+
+srun python /home/h6/leve469a/car_following/training/hp_search.py $SLURM_ARRAY_TASK_ID
+
+
--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@
 #SBATCH --nodes=1
 #SBATCH --time=48:00:00
 #SBATCH -J jobname
-#SBATCH --error="/home/leve469a/myjob-%J.out"
-#SBATCH --output="/home/leve469a/myjob-%J.out"
+#SBATCH --error="/home/leve469a/maxent-%J.out"
+#SBATCH --output="/home/leve469a/maxent-%J.out"
 
 ml purge
 ml release/24.04 GCCcore/11.3.0
