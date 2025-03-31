0e27ccc2011ca7a86a939fd26a3a18787e5471ad
llmvetter <lennart.vetter@yahoo.de>
2025-03-31 14:19:51 +0200

    Implements downsampling to 10hz

--- src/models/preprocessor.py
+++ src/models/preprocessor.py
@@ -3,69 +3,13 @@ from omegaconf import OmegaConf
 from sklearn.model_selection import train_test_split
 
 from src.models.trajectory import Trajectory, Trajectories
-from src.models.mdp import CarFollowingMDP
-
-
-class Preprocessor():
-    def __init__(
-            self,
-            mdp: CarFollowingMDP,
-            config: OmegaConf,
-    ):
-        self.mdp = mdp
-        self.min_speed = config.data.speed_treshold
-
-    def create_filtered_trajectory(
-            self,
-            df: pd.DataFrame,
-            expert_num: int,
-            min_speed: int,
-    ) -> Trajectory:
-        filtered_df = df[df[f'expert{expert_num}_speed'] > min_speed].copy()
-        filtered_df = filtered_df.reset_index(drop=True)
-        
-        return Trajectory(
-            speed=filtered_df[f'expert{expert_num}_speed'],
-            distance=filtered_df[f'expert{expert_num}_distance'],
-            acceleration=filtered_df[f'expert{expert_num}_acceleration'],
-            mdp=self.mdp,
-        )
-
-    def load(
-            self,
-            path: str,
-
-    ) -> Trajectories:
-        df = pd.read_csv(path, sep='\t', header=None)
-        df['expert1_acceleration'] = df[0].diff().shift(-1)
-        df['expert2_acceleration'] = df[1].diff().shift(-1)
-        df['expert3_acceleration'] = df[2].diff().shift(-1)
-        df['expert4_acceleration'] = df[3].diff().shift(-1)
-        df = df.dropna()
-        df = df.reset_index(drop=True)
-        df.rename(columns={
-            0: 'expert1_speed',
-            1: 'expert2_speed',
-            2: 'expert3_speed',
-            3:'expert4_speed',
-            4:'expert1_distance',
-            5:'expert2_distance',
-            6:'expert3_distance'
-            }, 
-            inplace=True,
-        )
-        
-        trajectory1 = self.create_filtered_trajectory(df, 1, self.min_speed)
-        trajectory2 = self.create_filtered_trajectory(df, 2, self.min_speed)
-        trajectory3 = self.create_filtered_trajectory(df, 3, self.min_speed)
-        trajectories = Trajectories([trajectory1, trajectory2, trajectory3])
-        return trajectories
+from src.models.env import CarFollowingEnv
 
 
 class MilanoPreprocessor:
     def __init__(
             self,
-            mdp: CarFollowingMDP,
+            mdp: CarFollowingEnv ,
             config: OmegaConf,
     ) -> None:
         self.kmh_to_ms = 0.27778
@@ -73,7 +17,7 @@ class MilanoPreprocessor:
         self.min_speed = config.data.speed_treshold
         self.random_state = config.data.random_state
 
-    def _filter_leader_follower_pairs(self, df: pd.DataFrame, min_entries: int = 1000) -> pd.DataFrame:
+    def _filter_leader_follower_pairs(self, df: pd.DataFrame, min_entries: int = 330) -> pd.DataFrame:
         """
         Filters leader-follower pairs that have at least `min_entries` data points.
         """
@@ -111,7 +55,8 @@ class MilanoPreprocessor:
         """
         Loads and processes the dataset to extract leader-follower trajectories.
         """
-        df = pd.read_csv(path)
+        df_init = pd.read_csv(path)
+        df = df_init.iloc[::3].reset_index(drop=True)  # downsample to 10hz
         df["Follower Speed"] *= self.kmh_to_ms
         df["Relative speed"] *= self.kmh_to_ms
 
